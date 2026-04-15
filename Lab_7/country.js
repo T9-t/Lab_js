2624aece6429e77c4a660ff42022cb6904e7907c
T9 <veronggg5@gmail.com>
2026-04-16 01:02:34 +0700

    docs: delete docs

--- Lab_7/country.js
+++ Lab_7/country.js
@@ -1,13 +1,3 @@
-
-/*<label for="country">Страна:</label> <br>
-<input id="country" onchange="check()"></input> //сделать выборочным полем через js
-
-<select id="color">
-    <option value="red">red</option>
-    <option value="blue">blue</option>
-    <option value="green">green</option>
-</select>*/
-
 let countrySelect = document.getElementById("country_input");
 
 let countryRu = ["Россия", "Китай", "Индия", "Германия", 
